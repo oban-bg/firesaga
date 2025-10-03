b08d143cd20b15bf24499b0b740c44d4f8a3db13
jam06452 <danjward8@gmail.com>
2025-10-03 12:49:31 +0100

    API TTI Fix

--- config/runtime.exs
+++ config/runtime.exs
@@ -4,5 +4,5 @@ config :fire_saga,
   openai_ttt: System.get_env("OPENAI_TTT_URL") || "https://api.openai.com/v1/chat/completions",
   openai_tti: System.get_env("OPENAI_TTI_URL") || "https://api.openai.com/v1/images/generations",
   openai_ttt_api: System.get_env("OPENAI_TTT_API"),
-  openai_tti_api: System.get_env("OPENAI_TTI_API") || openai_ttt_api,
+  openai_tti_api: System.get_env("OPENAI_TTI_API"),
   openai_org_id: System.get_env("OPENAI_ORG_ID")
