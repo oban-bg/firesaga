8a47c5c40a1c4672fde88ca6191121c2b1217716
jam06452 <danjward8@gmail.com>
2025-10-03 12:45:55 +0100

    Allowed for different api token and endpoints to work

--- lib/fire_saga/llm.ex
+++ lib/fire_saga/llm.ex
@@ -1,8 +1,7 @@
 defmodule FireSaga.LLM do
   def chat!(prompt) do
     body = %{model: "gpt-4o-mini", messages: [%{role: "user", content: prompt}]}
-
-    case post(json: body, url: Application.fetch_env!(:fire_saga, :openai_ttt)) do
+    case post(:ttt, json: body) do
       {:ok, %{body: body}} -> get_in(body, ["choices", Access.at(0), "message", "content"])
       error -> raise error
     end
@@ -10,25 +9,33 @@ defmodule FireSaga.LLM do
 
   def image!(prompt) do
     body = %{model: "dall-e-3", prompt: prompt, size: "1024x1024"}
-
-    case post(json: body, url: Application.fetch_env!(:fire_saga, :openai_tti)) do
+    case post(:tti, json: body) do
       {:ok, %{body: body, status: 200}} -> get_in(body, ["data", Access.at(0), "url"])
       error -> raise error
     end
   end
 
-  defp post(opts) do
-    api_key = Application.fetch_env!(:fire_saga, :openai_api_key)
-    oorg_id = Application.fetch_env!(:fire_saga, :openai_org_id)
+  defp post(:ttt, opts) do
+    api_key = Application.fetch_env!(:fire_saga, :openai_ttt_api)
+    url = Application.fetch_env!(:fire_saga, :openai_ttt)
+    post_request(opts, api_key, url)
+  end
 
+  defp post(:tti, opts) do
+    api_key = Application.fetch_env!(:fire_saga, :openai_tti_api)
+    url = Application.fetch_env!(:fire_saga, :openai_tti)
+    post_request(opts, api_key, url)
+  end
+
+  defp post_request(opts, api_key, url) do
+    oorg_id = Application.fetch_env!(:fire_saga, :openai_org_id)
     defaults = [
+      url: url,
       auth: {:bearer, api_key},
       headers: %{"openai-organization" => oorg_id},
       receive_timeout: 60_000
     ]
-
     app_opts = Application.get_env(:fire_saga, __MODULE__, [])
-
     opts
     |> Keyword.merge(defaults)
     |> Keyword.merge(app_opts)
